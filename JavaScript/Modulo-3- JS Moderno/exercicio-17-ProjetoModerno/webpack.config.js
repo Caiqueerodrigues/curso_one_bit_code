88b3b351362b796774565e5e6373d3c1364e36c3
Caiqueerodrigues <110926789+Caiqueerodrigues@users.noreply.github.com>
2023-02-13 10:55:33 -0300

    modulo finalizado

---
+++ JavaScript/Modulo-3- JS Moderno/exercicio-17-ProjetoModerno/webpack.config.js
@@ -0,0 +1,27 @@
+const { dirname } = require('path')
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    }
+  },
+  entry: {
+    index: './src/index.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.js$/,
+      use: ['babel-loader']
+    }]
+  },
+  output: {
+    filename: '[name].min.js'
+  }
+}
+
