52e4fbda621c8d8146107d1a97cb45a2199c426e
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-03-08 15:46:19 -0300

    setInterval()

--- JavaScript/modulo-5-Avaçado/setTimeout-setInterval/index.js
+++ JavaScript/modulo-5-Avaçado/setTimeout-setInterval/index.js
@@ -1,5 +1,16 @@
 console.log(`Hello, world`)
 
-setTimeout( () => {
-    console.log(`passados 3seg desde o inicio do programa`)
-}, 1000 * 3)
+// setTimeout( () => {
+//     console.log(`passados 3seg desde o inicio do programa`)
+// }, 1000 * 3)
+
+let seconds = 0
+const id = setInterval( () => {
+    seconds ++ 
+    console.log(`Se passaram ${seconds} segundos`)
+    if(seconds > 10) {
+        clearInterval(id)
+        console.log(`tempo máximo de ${seconds} segundos ATINGIDO, Encerrando. . .`)
+    }
+}, 1000)
+
