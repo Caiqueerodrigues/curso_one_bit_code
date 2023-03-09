0bcf0a520e9872900b4e49c1203a53aeb2e155f8
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-03-09 17:14:43 -0300

    concluido

--- JavaScript/modulo-5-Avaçado/exercicio-refatorando/index.js
+++ JavaScript/modulo-5-Avaçado/exercicio-refatorando/index.js
@@ -1,48 +1,40 @@
 async function imc(peso, altura) {
     if(typeof peso != `number` || typeof altura != `number`) {
         if( typeof altura != `number`) {
-            return rejected(`[ERRO], Valor informado para ALTURA NÃO é um Número.`)
+            return Promise.reject(`[ERRO], Valor informado para ALTURA NÃO é um Número.`)
         } else {
-             return rejected(`[ERRO], Valor informado para PESO NÃO é um Número.`)
+             return Promise.reject(`[ERRO], Valor informado para PESO NÃO é um Número.`)
         }
     } else {
         return peso / (altura * altura) 
     }
 }
 
-function resultado(peso, altura) {
-    setTimeout( () => {
-        imc(peso,altura).then( (result) => {
-            if(result < 18.5) {
-                console.log(`Seu IMC foi de: ${result} = MAGREZA`)
-                return
-            }
-            if (result >= 18.5 && result <= 24.9) {
-                console.log(`Seu IMC foi de: ${result} = NORMAL`)
-                return
-            }
-            if (result >= 25 && result <= 29.9) {
-                console.log(`Seu IMC foi de: ${result} = SOBREPESO`)
-                return
-            }
-            if (result >= 30 && result <= 39.9) {
-                console.log(`Seu IMC foi de: ${result} = OBESIDADE`)
-                return
-            }
-            if (result >= 40) {
-                console.log(`Seu IMC foi de: ${result} = OBESIDADE GRAVE`)
-                return
-            }
-        }).catch( (error) => {
-            console.log(`a promise foi rejeitada pelo Motivo: ${error}`)
-        }).finally ( () => {
-            setTimeout( () => {
-                console.log(`A aplicação foi Encerrada`)
-            }, 1000 * 2)
-        })
-    }, 1000 * 3)
-
-    console.log(`a segunda funçao foi chamada`)
+async function resultado(peso, altura) {
+    try {
+        console.log(`calculando IMC peso: ${peso} e altura ${altura}`)
+        const resultadoImc = await imc(peso,altura)
+        if(resultadoImc < 18.5) {
+            console.log(`Seu IMC foi de: ${resultadoImc} = MAGREZA`)
+        } else if (resultadoImc <= 24.9) {
+            console.log(`Seu IMC foi de: ${resultadoImc} = NORMAL`)
+        } else if (resultadoImc <= 29.9) {
+            console.log(`Seu IMC foi de: ${resultadoImc} = SOBREPESO`)
+        } else if (resultadoImc <= 39.9) {
+            console.log(`Seu IMC foi de: ${resultadoImc} = OBESIDADE`)
+        } else {
+            console.log(`Seu IMC foi de: ${resultadoImc} = OBESIDADE GRAVE`)
+            return
+        }
+    } 
+    catch(error) {
+        console.log(`a promise foi rejeitada pelo Motivo: ${error}`)
+    } 
+    finally {
+        setTimeout( () => {
+            console.log(`A aplicação foi Encerrada`)
+        }, 1000 * 2)
+    }
 }
 
 resultado(70,1.80)
