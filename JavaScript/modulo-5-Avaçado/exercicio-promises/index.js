9162be196b6b89f5655dbcb046bb412ada93f00e
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-03-09 10:15:36 -0300

    bug na validacao da entrada dos parametros
    Nao esta verificando corretamente se sao numero

---
+++ JavaScript/modulo-5-Avaçado/exercicio-promises/index.js
@@ -0,0 +1,56 @@
+function imc(peso, altura) {
+    return calculoImc = new Promise( (resolved, rejected) => {
+        // if(peso != /\d{1,}(?=\.)|(?<=\.)\d{1,}/gi || altura != /\d{1,}(?=\.)|(?<=\.)\d{1,}/gi ) {
+        //     if( altura != /\d{1,}(?=\.)|(?<=\.)\d{1,}/gi) {
+        //         rejected(`[ERRO], Valor informado para ALTURA NÃO é um Número.`)
+        //         console.log(altura)
+        //     } else {
+        //         rejected(`[ERRO], Valor informado para PESO NÃO é um Número.`)
+        //         console.log(10)
+            // }
+        if(peso === /D/gi || altura === /D/gi){
+            rejected(`[ERRO], Valor informado para ALTURA NÃO é um Número.`)
+        }
+         else {
+            let alturaTotal = altura*altura
+            resolved(peso / alturaTotal)
+        }
+    })
+}
+
+function resultado(peso, altura) {
+    setTimeout( () => {
+        imc(peso,altura).then( (result) => {
+            if(result < 18.5) {
+                console.log(`Seu IMC foi de: ${result} = MAGREZA`)
+                return
+            }
+            if (result >= 18.5 && result <= 24.9) {
+                console.log(`Seu IMC foi de: ${result} = NORMAL`)
+                return
+            }
+            if (result >= 25 && result <= 29.9) {
+                console.log(`Seu IMC foi de: ${result} = SOBREPESO`)
+                return
+            }
+            if (result >= 30 && result <= 39.9) {
+                console.log(`Seu IMC foi de: ${result} = OBESIDADE`)
+                return
+            }
+            if (result >= 40) {
+                console.log(`Seu IMC foi de: ${result} = OBESIDADE GRAVE`)
+                return
+            }
+        }).catch( (error) => {
+            console.log(`a promise foi rejeitada pelo Motivo: ${error}`)
+        }).finally ( () => {
+            setTimeout( () => {
+                console.log(`A aplicação foi Encerrada`)
+            }, 1000 * 2)
+        })
+    }, 1000 * 3)
+
+    console.log(`a segunda funçao foi chamada`)
+}
+
+resultado(70,1.80)
