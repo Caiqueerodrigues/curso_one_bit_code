25047863bafeb0ec40ff45f5f965b2b9e6d3a3a8
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-03-11 10:02:53 -0300

    conhecendo e consumindo uma API

---
+++ JavaScript/modulo-5-Avaçado/requisicoes-get/index.js
@@ -0,0 +1,24 @@
+function createCard (pais) {
+    const card = document.createElement(`div`)
+        card.classList.add(`country`)
+    
+        const countryName = pais.name.common
+        const name = document.createElement(`h2`)
+            name.textContent = countryName
+
+        const flag = document.createElement(`img`)
+            flag.src = pais.flags.svg
+            flag.alt = `Bandeira ${name}`
+
+        card.append(name, flag)
+        document.getElementById(`countries`).append(card)
+}
+
+async function getCountries () {
+    const resposta = await fetch('https://restcountries.com/v3.1/all')
+    const paises = await resposta.json()
+    
+    paises.forEach(createCard)
+}
+
+getCountries()
