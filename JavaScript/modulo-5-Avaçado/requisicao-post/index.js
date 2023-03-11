a5a93f443bae51159d28fe72d6e1143549ca07cb
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-03-11 14:43:15 -0300

    primeira parte requisicao POST

--- JavaScript/modulo-5-Avaçado/requisicao-post/index.js
+++ JavaScript/modulo-5-Avaçado/requisicao-post/index.js
@@ -2,17 +2,22 @@ async function getArticle() {
     const result = await fetch(`http://localhost:3000/articles`)
     const article = await result.json()
     article.forEach(create);
+    console.log(article)
 }
 
-function create() {
+function create(article) {
     const div = document.createElement(`div`)
         div.classList.add(`div`)
+        div.id = article.id
     
-    const title = document.createElement(`h3`).textContent = article.title
+    const title = document.createElement(`h3`)
+        title.textContent = article.title
 
-    const p = document.createElement(`p`).textContent = article.content
+    const p = document.createElement(`p`)
+        p.textContent = article.content
 
-    const autor = document.createElement(`h6`).textContent = article.autor
+    const  autor = document.createElement(`h6`)
+        autor.textContent = article.autor
 
     div.append(title, p, autor)
     document.getElementById(`articles`).append(div)
