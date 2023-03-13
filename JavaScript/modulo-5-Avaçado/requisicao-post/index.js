79ff469969eb6c95d90d839fc171f638e248c58a
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-03-13 08:36:18 -0300

    fim

--- JavaScript/modulo-5-Avaçado/requisicao-post/index.js
+++ JavaScript/modulo-5-Avaçado/requisicao-post/index.js
@@ -22,5 +22,27 @@ function create(article) {
     div.append(title, p, autor)
     document.getElementById(`articles`).append(div)
 }
+const form = document.querySelector(`form`).addEventListener(`submit`, async (ev) => {
+    ev.preventDefault()
+
+    const dadosArtigo = {
+        title: document.querySelector(`#title`).value,
+        author: document.querySelector(`#author`).value,
+        content: document.querySelector(`#content`).value
+    }
+
+    const resposta = await fetch(`http://localhost:3000/articles`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        //converter o obj para string
+        body: JSON.stringify(dadosArtigo)
+    })
+
+    const artigoSalvo = await resposta.json()
+    form.reset()
+    create(artigoSalvo)
+})
 
 getArticle()
