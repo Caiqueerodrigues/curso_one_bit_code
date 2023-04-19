3bd2df843966453b79c8b0d9facd5c9d0eb6e661
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-04-19 10:40:26 -0300

    exercicio finalizado

--- JavaScript/modulo-4-POO/exercicio-classe-doom/index.js
+++ JavaScript/modulo-4-POO/exercicio-classe-doom/index.js
@@ -4,5 +4,27 @@ import { Input } from "./input.js"
 import { Label } from "./label.js"
 
 const title = new Component(`h1`, `body`, { textContent: `Olá, Mundo!`})
-
+title.render()
 console.log(title)
+
+title.tag='h4'
+title.build().render()
+
+const form  = new Form ('body')
+
+const label = new Label('Nome', form, {htmlfor: 'inputName'})
+
+const input = new Input(form, {id: 'nameInput', name: 'name'})
+
+form.render()
+label.render()
+form.addChildren(new Component('br'),new Component('br'),input)
+
+form.addChildren(
+    new Component('br'),
+    new Component('br'),
+    new Label('Data de Nascimento', {htmlfor:'birthday'}),
+    new Component('br'),
+    new Component('br'),
+    new Input(form, {id:'birthday', type:'date'})
+)
