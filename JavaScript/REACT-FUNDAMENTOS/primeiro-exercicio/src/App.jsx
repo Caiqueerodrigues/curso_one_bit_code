d89955c9d298ca6e98438b94b310612ec1becd0d
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-05-11 16:35:53 -0300

    iniciando REACT

---
+++ JavaScript/REACT-FUNDAMENTOS/primeiro-exercicio/src/App.jsx
@@ -0,0 +1,28 @@
+export default function App() {
+  return (
+    <div>
+      <picture>
+        <img src="/react-logo.png" alt="Logo do React" />
+      </picture>
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuário web e nativas.</p>
+      <button>Aprenda React</button> <button>Referência da API</button>
+      <hr />
+      <h1>Crie interafces de usuários de componentese</h1>
+      <p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados components.</p>
+      <hr />
+      <h1>Escreve componentes com código e marcação</h1>
+      <p>Componentes React são funções JavaScript. a sintaxe de marcação é chamada de JSX. É uma extensão da sintaxe JavaScript popularizada pelo React.</p>
+      <hr />
+      <h1>Próximos passos:</h1>
+      <ul>
+        <li>Uso de dados dinâmicos no JSX</li>
+        <li>criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Reutilização de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos do JavaScript</li>
+      </ul>
+    </div>
+  )
+}
