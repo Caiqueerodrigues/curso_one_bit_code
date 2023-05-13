87e62479372ed59075216127bfaa26535102485f
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-05-12 22:01:51 -0300

    pedido finalizado

--- JavaScript/projeto-pedido/index.js
+++ JavaScript/projeto-pedido/index.js
@@ -2,13 +2,27 @@ let btnSim = document.getElementById(`sim`)
 let btnNao = document.getElementById(`nao`)
 
 let title = document.querySelector(`h1`)
+let topBottom = 0
+let leftRight = 0
+
+let width = window.innerWidth
 
 function ativar () {
-    if (btnSim.innerText !== `sim`) {
-        btnSim.innerText = `Sim`
+    calcular()
+    if(topBottom < window.innerHeight && leftRight < window.innerWidth) {
+        btnSim.style.position=`absolute`
+        btnSim.style.display=`block`
+        btnSim.style.margin= `${(topBottom/2)}px ${(leftRight/2)}px`    
+        console.log(topBottom, leftRight)
+        console.log(`${window.innerHeight} height width ${window.innerWidth}`)
+    } else {
+        ativar()
+        console.log('maior')
     }
-    let position = Math.floor(Math.random() *163)
-    btnSim.style.display=`block`
-    btnSim.style.margin= `${position}px`
-    alert(position)
+    
+}
+
+function calcular() {
+    topBottom = Math.floor(Math.random() *2813)
+    leftRight = Math.floor(Math.random() *1603)
 }
