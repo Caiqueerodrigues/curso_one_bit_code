248afef6d3326413feb6874ac808ae81ad14158b
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-10-15 17:38:32 -0300

    devolvendo json com express

--- BACKEND/toDoList/app.js
+++ BACKEND/toDoList/app.js
@@ -6,6 +6,11 @@ app.get('/',(req, res) => { //1° arg é a rota, 2° requisição, 3° resposta
     res.send('<h1>Minha lista de tarefas :D</h1>'); // enviando a resposta da req
 })
 
+app.get('/json', (req, res) => {
+    //só acrescentar o JSON
+    res.json({title: "Tarefa X", done: true}) //retorna OBJ.
+})
+
 app.listen(3000, () => {
     console.log("Servidor iniciou")
 })
