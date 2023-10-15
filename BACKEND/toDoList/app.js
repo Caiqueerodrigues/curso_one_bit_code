c727d5ab4ebfd4120ff433e88419d929f11badaa
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-10-15 17:33:21 -0300

    adicionado nodemon

--- BACKEND/toDoList/app.js
+++ BACKEND/toDoList/app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 
+const app = express();
+
+app.get('/',(req, res) => { //1° arg é a rota, 2° requisição, 3° resposta
+    res.send('<h1>Minha lista de tarefas :D</h1>'); // enviando a resposta da req
+})
+
+app.listen(3000, () => {
+    console.log("Servidor iniciou")
+})
