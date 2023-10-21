701990b9dcae4fd57f252ad18da84b6f06e23d51
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-10-21 19:28:31 -0300

    metodos criados no back, verbos funcionando

--- BACKEND/toDoList/src/routes/checkList.js
+++ BACKEND/toDoList/src/routes/checkList.js
@@ -2,14 +2,18 @@ const express = require('express');
 
 const router = express.Router(); //do próprio express
 
+const Checklist = require("../models/checklist.js");
 
-router.get('/', (req, res) => { 
+router.get('/', async (req, res) => { 
     //parametro da rota, vem na chamada
-    console.log("Olá");
-    res.end();
+    try {
+        let checklists = await Checklist.find({});
+        res.status(200).json(checklists);
+    } catch(e) {
+        res.status(500).json(e)
+    }
 })
 
-const Checklist = require("../models/checklist.js");
 router.post('/', async (req, res) => { 
     //o express diferencia a chamada pelo VERBO
     let { name } = req.body;
@@ -22,20 +26,42 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     //dois pontos indica parametro
-    console.log(req.params.id)
-    res.send(`ID: ${req.params.id}`)
+    const id = req.params.id;
+    try {
+        let checkList = await Checklist.findById(id);
+        res.status(200).json(checkList);
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
-router.put('/:id', (req, res) => {
-    console.log(req.params.id)
-    res.send(`PUT ID: ${req.params.id}`)
+router.put('/:id', async (req, res) => {
+    let { name } = req.body;
+    const id = req.params.id;
+    
+    try {
+        //findByIdAndUpdate - encontra e já modifica
+        let checkList = await Checklist.findByIdAndUpdate(id, { name }, {new: true});
+        //objeto new: true é ára retornar o objeto modificado
+        //sem ele, retorna antes de modificar
+        res.status(200).json(checkList);
+    } catch (er) {
+        res.status(422).json(e)
+    }
 })
 
-router.delete('/:id', (req, res) => {
-    console.log(req.params.id)
-    res.send(`DELETE ID: ${req.params.id}`)
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    
+    try {
+        //findByIdAndRemove - encontra e já remove
+        let checkList = await Checklist.findByIdAndRemove(id);
+        res.status(200).json(checkList);
+    } catch (e) {
+        res.status(422).json(e)
+    }
 })
 
 module.exports = router;
