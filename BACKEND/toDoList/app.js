5aa8149c1143dd96cf5a54af5c9cd14dc1f9c73b
Caique Rodrigues <caiquerodriguesalmeida@gmail.com>
2023-10-22 18:58:44 -0300

    conhecendo e aplicando EJS

--- BACKEND/toDoList/app.js
+++ BACKEND/toDoList/app.js
@@ -1,12 +1,23 @@
 require('./config/dataBase.js');
 const express = require('express');
+
+const path = require('path');
+
 const checkListsRouter = require("./src/routes/checkList.js");
+const rootRouter = require("./src/routes/index.js");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
+//dizendo ao app que os arquivos estaticos estarão em PUBLIC
+
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs') //mostrar o engine
+//serve para inserir dados mais facil
 
 app.use('/checklists', checkListsRouter) //rota externa
+app.use('/', rootRouter) //rota externa
 
 app.listen(3000, () => {
     console.log("Servidor iniciou");
